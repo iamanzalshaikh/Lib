b17d63f1e48f84f72d680071227cf3ce5786480b
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-08 17:12:27 +0530

    Update vite.config.js

--- Libarary/frontend/vite.config.js
+++ Libarary/frontend/vite.config.js
@@ -5,10 +5,10 @@
 //   plugins: [react()],
 //   base: './' // important for Netlify hosting
 // });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './', // ensures assets load relative to index.html
 });
