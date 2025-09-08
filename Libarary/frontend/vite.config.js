0b3118260488a5ee0676dd333415591e694b30b6
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-08 16:50:48 +0530

    Update vite.config.js

--- Libarary/frontend/vite.config.js
+++ Libarary/frontend/vite.config.js
@@ -10,6 +10,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Libarary/frontend/', // <- match your GitHub repo path
+  plugins: [react()]
 });
